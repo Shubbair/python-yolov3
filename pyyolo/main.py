299873c748a613f5a221fe83c6253f1dbad65407
HussainSalih <45163499+HussainSalihMahde@users.noreply.github.com>
2019-12-21 00:39:30 +0300

    Add files via upload

---
+++ pyyolo/main.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+net =  cv2.dnn.readNet('yolov3.weights','yolov3.cfg')
+classes = []
+
+with open('coco.names','r') as f:
+    classes = [ line.strip() for line in f.readlines() ]
+
+layer_names = net.getLayerNames()
+outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+colors= np.random.uniform(0,255,size=(len(classes),3))
+
+font = cv2.FONT_HERSHEY_PLAIN
+
+#source of your image
+img = cv2.imread('nature.jpg')
+
+height,width,channels = img.shape
+blob = cv2.dnn.blobFromImage(img,0.00392,(416,416),(0,0,0),True,crop=False)
+
+net.setInput(blob)
+outs = net.forward(outputlayers)
+class_ids=[]
+confidences=[]
+boxes=[]
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            #onject detected
+            center_x= int(detection[0]*width)
+            center_y= int(detection[1]*height)
+            w = int(detection[2]*width)
+            h = int(detection[3]*height)
+
+            x=int(center_x - w/2)
+            y=int(center_y - h/2)
+            
+            boxes.append([x,y,w,h]) #put all rectangle areas
+            confidences.append(float(confidence)) #how confidence was that object detected and show that percentage
+            class_ids.append(class_id) #name of the object tha was detected
+
+indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.6)
+
+font = cv2.FONT_HERSHEY_PLAIN
+for i in range(len(boxes)):
+    if i in indexes:
+        x,y,w,h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[i]
+        cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
+        cv2.putText(img,label,(x,y+30),font,1,(255,255,255),2)
+            
+cv2.imshow("Image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
